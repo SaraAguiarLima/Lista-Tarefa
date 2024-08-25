3feaf8362c1f6bd78b6531a3e5e6292012ab991e
Sara Aguiar de Lima <119362975+SaraAguiarLima@users.noreply.github.com>
2024-08-25 11:36:46 -0300

    .

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@
 [X] Saber qndo o botão foi clicado -> function adicionarTarefa(){}
 [X] Pegar o texto dentro do input
 [X] Colocar esse texto na tela
-[] Deletar a tarefa  da tela(qndo clicar no X)
+[X] Deletar a tarefa  da tela(qndo clicar no X)
 */
 
 
@@ -17,15 +17,15 @@ function adicionarTarefa(){
     /* let variavel*/ /*input dou um nome para essa variavel*//*document ->leva até o HTML, .querySelector irá selecionar algo que eu queira no HTML, dentro dos parenteses com aspas duplas coloco o item que desejo que ele me traga, neste caso ("input"), como só tem um não tem problema, mas caso ajam mais ele trará o primeiro que encontrar, .value-> vai trazer somente o valor dentro do input */
 
     let li = document.createElement('li')
-    /*Criar outra variavel para fazer as LI não no HTML mas qui no JS, .createElement-> vai criar elementos, -> crio uma ("li")*/
+    /*Criar outra variavel para fazer as LI não no HTML mas aqui no JS, .createElement-> vai criar elementos, -> crio uma ("li")*/
     li.innerHTML = valorDoInput + '<span onclick="deletarTarefa(this)">❌</span>' /*.innerHTML-> vai colocar algo dentro*/
 
     document.querySelector('ul').appendChild(li)/*appendChild-> cria um filho dentro do elemento*/
 
-    valorDoInput = ''/*Toda vez que uma tarefa for adicionada o valor de input vai ficar vazio para receber outra tarefa*/
+    document.querySelector("input").value='' // vai fazer apagar o que foi digitado dando espaço a nova tarefa
 
 }
 
 function deletarTarefa(li) {
-    console.log(li)
+   li.parentElement.remove()
 }
